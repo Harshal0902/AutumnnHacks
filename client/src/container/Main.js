81dbdd68444ade505abf675d7b4489af183f8e3c
Harshal0902 <harshalraikwar07@gmail.com>
2022-09-12 20:12:45 +0530

    ✨ Add components

---
+++ client/src/container/Main.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from '../components/Navbar';
+
+function Main() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                {/* <Route path="/" element={<Home />} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Main;
